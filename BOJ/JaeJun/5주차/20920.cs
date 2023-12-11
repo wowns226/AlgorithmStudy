06c098828534ebc5f714dda532ce35f00faaaec0
LimJaeJun <wowns226@naver.com>
2023-12-11 12:14:09 +0900

    1. 4주차 5주차 6주차 문제 풀이 코드

---
+++ BOJ/JaeJun/5주차/20920.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace BOJ_20920
+{
+    class Program
+    {
+        static void Main()
+        {
+            using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            
+            StringBuilder sb = new StringBuilder();
+
+            int[] inputs = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            int n = inputs[0];
+            int m = inputs[1];
+
+            for (int i = 0; i < n; i++)
+            {
+                string s = sr.ReadLine();
+ 
+                if (s.Length < m) continue;
+
+                dict.TryAdd(s, 0);
+
+                dict[s]++;
+            }
+
+            var sorted = dict.OrderByDescending(x => x.Value).ThenByDescending(x => x.Key.Length).ThenBy(x => x.Key).ToDictionary(x=>x.Key,x=>x.Value);
+
+            foreach (var item in sorted)
+            {
+                sb.AppendLine(item.Key);
+            }
+
+            sw.Write(sb);
+        }
+    }
+}
